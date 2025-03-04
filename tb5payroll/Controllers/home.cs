7763901c40426017ded8f92a1104016b8a36e79a
jj713 <jj713@LAPTOP-EKT9C78D>
2025-03-04 15:29:58 +0800

    print

---
+++ tb5payroll/Controllers/home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tb5payroll.Controllers
+{
+    public class home : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
